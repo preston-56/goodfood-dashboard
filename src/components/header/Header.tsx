d1739948270eb2e3c1a5f4d06cf069f2d2af1272
Preston <prestonosoro56@gmail.com>
2024-04-24 16:10:26 -0700

    header component

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header className="text-dark">
+      <div>
+        <h1 className="text-2xl font-bold">Dashboard</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
