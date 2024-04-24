3ab31c7e081d97f5dbcd815b8de174dd509379a2
Preston <prestonosoro56@gmail.com>
2024-04-24 16:01:39 -0700

    import dashboard

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Dashboard from "./pages/dashboard/Dashboard";
+
+function App() {
+  return (
+    <div className="mx-w mx-auto px-8 py-8 text-center">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
