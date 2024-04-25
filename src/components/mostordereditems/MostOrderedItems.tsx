46b34d49c67cdcd171aadaa120eaccd799570404
Preston <prestonosoro56@gmail.com>
2024-04-25 06:24:12 -0700

    import assets and render

--- src/components/mostordereditems/MostOrderedItems.tsx
+++ src/components/mostordereditems/MostOrderedItems.tsx
@@ -1,84 +1,37 @@
 import React from "react";
+import { mostOrderedItems } from "../../data";
+import NoodlesImage from "../../assets/noodles.jpg";
+import BurgerImage from "../../assets/burger.jpg";
+import SaladImage from "../../assets/salad.jpg";
+import SmoothieImage from "../../assets/smoothie.jpg";
+import WingsImage from "../../assets/chicken.jpg";
+
+const images: { [key: string]: string } = {
+  "Vegatable Burger": BurgerImage,
+  "Fresh Salad Bowl": SaladImage,
+  "Smoothie Fruits": SmoothieImage,
+  "Hot Chicken Wings": WingsImage,
+  "Chicken Noodles": NoodlesImage,
+};
 
 const MostOrderedItems: React.FC = () => {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-md">
+    <div className="bg-white p-4 rounded-lg shadow-md mb-4">
       <div className="flex flex-col">
         <h2 className="text-xl font-bold mb-4 grid justify-items-start">Most Ordered Food</h2>
         <p className="text-gray-500 text-start mb-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          malesuada...
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed malesuada...
         </p>
-        <div className="mb-4">
-          <hr className="my-2" />
-          <div className="flex items-center space-x-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 text-gray-400"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 2a8 8 0 100 16 8 8 0 000-16zM6 10a1 1 0 011-1h6a1 1 0 010 2H7a1 1 0 01-1-1zm8-3a1 1 0 00-1-1h-1.586a1 1 0 110-2H14a2 2 0 012 2zm0 6a1 1 0 01-1 1h-1.586a1 1 0 100 2H14a2 2 0 001-1V9zm-8-2a2 2 0 100 4 2 2 0 000-4zm0-2a4 4 0 100 8 4 4 0 000-8zm0-2a6 6 0 100 12 6 6 0 000-12z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <span>Fresh Salad Bowl</span>
-            <span className="ml-2">IDR 45.000</span>
-          </div>
-          <hr className="my-2" />
-          <div className="flex items-center space-x-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 text-gray-400"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 2a8 8 0 100 16 8 8 0 000-16zM6 10a1 1 0 011-1h6a1 1 0 010 2H7a1 1 0 01-1-1zm8-3a1 1 0 00-1-1h-1.586a1 1 0 110-2H14a2 2 0 012 2zm0 6a1 1 0 01-1 1h-1.586a1 1 0 100 2H14a2 2 0 001-1V9zm-8-2a2 2 0 100 4 2 2 0 000-4zm0-2a4 4 0 100 8 4 4 0 000-8zm0-2a6 6 0 100 12 6 6 0 000-12z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <span>Chicken Noodles</span>
-            <span className="ml-2">IDR 75.000</span>
-          </div>
-          <hr className="my-2" />
-          <div className="flex items-center space-x-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 text-gray-400"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 2a8 8 0 100 16 8 8 0 000-16zM6 10a1 1 0 011-1h6a1 1 0 010 2H7a1 1 0 01-1-1zm8-3a1 1 0 00-1-1h-1.586a1 1 0 110-2H14a2 2 0 012 2zm0 6a1 1 0 01-1 1h-1.586a1 1 0 100 2H14a2 2 0 001-1V9zm-8-2a2 2 0 100 4 2 2 0 000-4zm0-2a4 4 0 100 8 4 4 0 000-8zm0-2a6 6 0 100 12 6 6 0 000-12z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <span>Smoothie Fruits</span>
-            <span className="ml-2">IDR 45.000</span>
-          </div>
-          <hr className="my-2" />
-          <div className="flex items-center space-x-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5 text-gray-400"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10 2a8 8 0 100 16 8 8 0 000-16zM6 10a1 1 0 011-1h6a1 1 0 010 2H7a1 1 0 01-1-1zm8-3a1 1 0 00-1-1h-1.586a1 1 0 110-2H14a2 2 0 012 2zm0 6a1 1 0 01-1 1h-1.586a1 1 0 100 2H14a2 2 0 001-1V9zm-8-2a2 2 0 100 4 2 2 0 000-4zm0-2a4 4 0 100 8 4 4 0 000-8zm0-2a6 6 0 100 12 6 6 0 000-12z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <span>Hot Chicken Wings</span>
-            <span className="ml-2">IDR 45.000</span>
-          </div>
-        </div>
+        {mostOrderedItems.map((item, index) => (
+          <React.Fragment key={index}>
+            <div className="flex items-center space-x-4">
+              <img src={images[item.name]} alt={item.name} className="mt-1 h-10 w-10 rounded-full" />
+              <div>{item.name}</div>
+              <div style={{ marginLeft: "auto" }}>${item.price.toFixed(3)}</div>
+            </div>
+            <hr className="border-t border-gray-300 w-full my-1" />
+          </React.Fragment>
+        ))}
       </div>
     </div>
   );
