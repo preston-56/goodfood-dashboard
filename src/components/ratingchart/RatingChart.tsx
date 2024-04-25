bf37c39d1adae69bf32435ab64fe0829fa3e80d5
Preston <prestonosoro56@gmail.com>
2024-04-25 06:26:49 -0700

    adjust rating chart width

--- src/components/ratingchart/RatingChart.tsx
+++ src/components/ratingchart/RatingChart.tsx
@@ -1,29 +1,42 @@
-import React from "react";
+import { LineChart, lineElementClasses, markElementClasses } from '@mui/x-charts/LineChart';
+import { ratingData } from '../../data';
 
-const RatingChart: React.FC = () => {
+const RatingLineChart = () => {
+  // Extracting aspect names and ratings from the data
+  const aspects = ratingData.map(({ aspect }) => aspect);
+  const ratings = ratingData.map(({ rating }) => rating);
+
   return (
-    <div className="bg-white p-6 rounded-lg shadow-md">
-      <div className="flex flex-col">
-        <h2 className="flex justify-items-start text-xl font-bold mb-4">Your Rating</h2>
-        <p className="text-gray-500 text-start mb-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          malesuada...
-        </p>
-        <div className="flex space-x-4">
-          {/* Icons with circles representing ratings */}
-          <div className="bg-gray-200 rounded-full h-10 w-10 flex items-center justify-center">
-            85% Hygiene
-          </div>
-          <div className="bg-gray-200 rounded-full h-10 w-10 flex items-center justify-center">
-            85% Food Taste
-          </div>
-          <div className="bg-gray-200 rounded-full h-10 w-10 flex items-center justify-center">
-            92% Packaging
-          </div>
-        </div>
-      </div>
+    <div className="flex flex-col">
+
+      <h2 className="text-xl font-bold mb-4 grid justify-items-start">Food Ratings</h2>
+      <p className="text-gray-500 text-start mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+        malesuada...
+      </p>
+      <LineChart
+        width={500}
+        height={300}
+        series={[{ data: ratings, label: 'Ratings', id: 'ratingsId' }]}
+        xAxis={[{ scaleType: 'point', data: aspects }]}
+        sx={{
+          [`.${lineElementClasses.root}, .${markElementClasses.root}`]: {
+            strokeWidth: 1,
+          },
+          '.MuiLineElement-series-ratingsId': {
+            strokeDasharray: '5 5',
+          },
+          [`.${markElementClasses.root}:not(.${markElementClasses.highlighted})`]: {
+            fill: '#fff',
+          },
+          [`& .${markElementClasses.highlighted}`]: {
+            stroke: 'none',
+          },
+        }}
+      />
     </div>
+
   );
 };
 
-export default RatingChart;
+export default RatingLineChart;
