c508bc8b4127025d0e1907199e4e0419a7075c42
Preston <prestonosoro56@gmail.com>
2024-04-25 06:25:34 -0700

    adjust chart width

--- src/components/ordertrendchart/OrderTrendChart.tsx
+++ src/components/ordertrendchart/OrderTrendChart.tsx
@@ -11,7 +11,7 @@ const OrderTrendChart: React.FC = () => {
     <div className="grid gap-y-6">
       <div className="flex flex-row justify-between items-center">
         <div className="grid justify-items-start">
-          <h2 className="text-xl font-bold">Order</h2>
+          <h2 className="text-xl font-bold">Order Trends</h2>
           <h1 className="text-xl font-bold">IDR 7.852.000</h1>
           <div className="flex items-center text-red-500">
             <ArrowDropDownIcon fontSize="small" />
@@ -27,33 +27,33 @@ const OrderTrendChart: React.FC = () => {
         Sales from 1-6 Dec, 2020
       </h4>
 
-     <div>
-     <LineChart
-        width={600}
-        height={300}
-        series={[
-          { data: ordersData, label: "This Week", color: "#757575" },
-          {
-            data: ordersData.slice(-7),
-            label: "Previous Week",
-            color: "#1565c0",
-          },
-        ]}
-      ></LineChart>
+      <div>
+        <LineChart
+          width={600}
+          height={300}
+          series={[
+            { data: ordersData, label: "This Week", color: "#757575" },
+            {
+              data: ordersData.slice(-7),
+              label: "Previous Week",
+              color: "#1565c0",
+            },
+          ]}
+        ></LineChart>
 
-      {/* labels */}
-      <div className="flex justify-normal ml-2">
-        <div className="flex items-center">
-          <div className="w-3 h-3 bg-blue-800 rounded-full mr-2"></div>
-          <span className="text-xs">Last 6 days</span>
-        </div>
-        <div className="flex items-center">
-          <div className="w-3 h-3 bg-gray-400 rounded-full mr-2"></div>
-          <span className="text-xs">Last Week</span>
+        {/* labels */}
+        <div className="flex justify-normal ml-2 mb-4">
+          <div className="flex items-center">
+            <div className="w-3 h-3 bg-blue-800 rounded-full mr-2"></div>
+            <span className="text-xs">Last 6 days</span>
+          </div>
+          <div className="flex items-center ml-4">
+            <div className="w-3 h-3 bg-gray-400 rounded-full mr-2"></div>
+            <span className="text-xs">Last Week</span>
+          </div>
         </div>
       </div>
     </div>
-     </div>
   );
 };
 
